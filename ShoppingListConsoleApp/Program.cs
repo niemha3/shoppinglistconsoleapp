b359072b8c15d0c0386d61a0065c6c2f4641939a
niemha3 <harri.nieminen22@gmail.com>
2022-06-08 10:43:42 +0300

    adding project files to remote repo

---
+++ ShoppingListConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace ShoppingListConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Harri\source\repos\ShoppingListConsoleApp\shoppinglistitems.txt";
+
+            while (true)
+            {
+            Console.WriteLine("Input a shopping item or \"quit\" to end: ");
+            string input = Console.ReadLine();
+                if(input != "quit")
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(input);
+            }
+
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+        }
+
+
+    }
+}
